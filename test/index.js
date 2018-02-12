0ae4f1a744f1a8dfb66197b23a584934d4ed8244
Esbjörn Blomquist <esbjorn.blomquist@cybercom.com>
2018-02-12 16:08:23 +0100

    get stringToElement exported as b4

--- test/index.js
+++ test/index.js
@@ -1,16 +1,16 @@
 // import style from './style';
 import { Component } from 'preact';
 // import Gauge, { createGauge } from '../index';
-import { createGauge } from '../index';
+import { createGauge, stringToElement } from '../index';
 
 export default class App extends Component {
 	updateGauge = () => {
 		const value = Math.random() * 130;
-		console.log('Setting:', value);
+		// console.log('Setting:', value);
 		this.gauge.setTarget(value);
 	};
 	componentDidMount() {
-		// console.log(Gauge, createGauge);
+		console.log(createGauge, stringToElement);
 		// const testG = new Gauge(this.area, { size: 300 });
 		this.gauge = createGauge(this.area, { size: 300 });
 		// console.log(this.gauge);
